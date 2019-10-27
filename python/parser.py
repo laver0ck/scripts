cb3df36fa1afbe6e201e9c6d4e1b2cbd6ceed408
laverock <they.all.alike@gmail.com>
2019-10-27 14:02:38 +0300

    add multiple files functionality to parser.py

--- python/parser.py
+++ python/parser.py
@@ -1,18 +1,18 @@
 import sys
+import os
 
 ''' TODO:
-    1. get log from jenkins job with requests(auth)
-    2. multiple files as arguments
-    3. wildcards (glob.glob)
-    4. get logs from range of jenkins jobs (e.g. 20-50) and parse them
+    - get log from jenkins job with requests(auth)
+    - wildcards (glob.glob)
+    - get logs from range of jenkins jobs (e.g. 20-50) and parse them
 '''
 
 usage = '''
-Usage: parser.py file_to_parse
-Only one file at a time, no wildcards
+Usage: parser.py file1 [file2...file_n]
+No wildcards supported yet
 '''
 
-if len(sys.argv) == 1 or len(sys.argv) > 2:
+if len(sys.argv) == 1:
     print('\nWrong arguments!')
     print(usage)
     exit(1)
@@ -20,17 +20,25 @@ elif sys.argv[1] and sys.argv[1] in ['-h', '--help']:
     print(usage)
     exit(1)
 
-with open(sys.argv[1]) as file:
-    task_name = ''
-    task_printed = False
-    for i in file.readlines():
-        if i.startswith("TASK ["):
-            task_name = i
-            task_printed = False
-        elif i.startswith('fatal:') or i.startswith('failed:'):
-            if task_printed is False:
-                print(task_name, end='')
-                print(i)
-                task_printed = True
-            else:
-                print(i)
+
+for file_name in sys.argv[1:]:
+
+    if not file_name.startswith('/'):
+        file_name = os.getcwd() + '/' + file_name
+
+    with open(file_name) as file:
+        print('===============================')
+        print('File: ' + file_name + ':\n')
+        task_name = ''
+        task_printed = False
+        for i in file.readlines():
+            if i.startswith("TASK ["):
+                task_name = i
+                task_printed = False
+            elif i.startswith('fatal:') or i.startswith('failed:'):
+                if task_printed is False:
+                    print(task_name, end='')
+                    print(i)
+                    task_printed = True
+                else:
+                    print(i)
